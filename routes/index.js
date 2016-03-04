d5c959c5c83c6e440fd46a145a638d0421c8e8d2
hannahtwiggsmith <htwiggsmith@gmail.com>
2016-03-04 18:04:31 -0500

    new route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('hellllooooooooo');
+});
+
+router.get('/config', function(req, res, next) {
+  res.send('hellooooo')
 });
 
 module.exports = router;
