f66a281ec49e4846d054d73e316c6b16c76eb814
Mimi <mimi@students.olin.edu>
2016-03-07 15:10:09 -0500

    weather API experiments

--- public/javascripts/indexController.js
+++ public/javascripts/indexController.js
@@ -1,4 +1,10 @@
-var app = angular.module('app', ['ngRoute']);
+var app = angular.module('loudCloud', ['ngRoute']);
+var apiURL;
+var data; = $.getJSON("http://api.openweathermap.org/data/2.5/weather?zip=91711&APPID=7df611ce3dfb9dd777f9f9816d8810c7&units=imperial", dataFetch)
+
+function dataFetch(data){console.log(data.weather[0].description)}
 
 app.controller('config', function($scope, $http){
-})
+	apiURL = "http://api.openweathermap.org/data/2.5/weather?zip=" + $scope.zipCode + "&APPID=7df611ce3dfb9dd777f9f9816d8810c7"
+ 	data = $.getJSON(apiURL, dataFetch)
+});
